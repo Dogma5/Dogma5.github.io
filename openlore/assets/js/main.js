d15b0c3457a4d287474854eefaac80d3a9f0de0c
TrebBrennan <treb.brennan@gmail.com>
2019-10-25 09:44:18 +1100

    Add jQuery and setup main nav
    Cause I'm not awesome enough at raw JS :(

--- openlore/assets/js/main.js
+++ openlore/assets/js/main.js
@@ -1,37 +1,29 @@
-var getNextSibling = function (elem, selector) {
-
-	// Get the next sibling element
-	var sibling = elem.nextElementSibling;
-
-	// If there's no selector, return the first sibling
-	if (!selector) return sibling;
-
-	// If the sibling matches our selector, use it
-	// If not, jump to the next sibling and continue the loop
-	while (sibling) {
-		if (sibling.matches(selector)) return sibling;
-		sibling = sibling.nextElementSibling
-	}
-
-};
-
-
-// Main nav dropdowns
-// var x = document.getElementsByClassName("");
-
-dropdownButtons = document.querySelectorAll('.dropdown-btn');
-
-var myFunction = function() {
-  var nextStop = getNextSibling(this, '.dropdown-box');
-
-  if ( window.getComputedStyle(nextStop).display === 'none' ){
-    nextStop.style.display = "block";
-  } else {
-    nextStop.style.display = "none";
-  }
-};
-
-
-for (var i = 0; i < dropdownButtons.length; i++) {
-    dropdownButtons[i].addEventListener('click', myFunction, false);
+function closeAll(){
+	$('.isopen').hide().removeClass('isopen');;
 }
+
+$( document ).ready(function() {
+
+	// Close all on HTML click
+	// ==================================
+	$(window).click(function(){
+		closeAll();
+	});
+
+	// Main manu dropdown
+	// ==================================
+	$('.dropdown-btn').click(function(){
+
+		mydropdown = $(this).next('.dropdown-box');
+
+		if ( mydropdown.is(":visible") ){
+			closeAll();
+		} else{
+			closeAll();
+			mydropdown.show().addClass('isopen');
+		}
+
+		event.stopPropagation();
+	});
+
+});// end doc ready
